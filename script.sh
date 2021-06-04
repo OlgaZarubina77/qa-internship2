9ba1315ac006254ad5ee97a10c3a316eb79129d3
Olga Zarubina <maomurdock77@gmail.com>
2021-06-04 11:09:47 +0300

    taskfix

--- script.sh
+++ script.sh
@@ -7,5 +7,5 @@ cp ./tasks/task3.txt ./tasks/task4.txt
 echo '***task4.txt***'
 cat ./tasks/task4.txt
 echo "This is the best script ever!"
-ps -e > ./tasks/task1.txt
-grep user ./tasks/task1.txt > ./tasks/task2.txt
+ps -fe > ./tasks/task1.txt
+grep "user" ./tasks/task1.txt > ./tasks/task2.txt
